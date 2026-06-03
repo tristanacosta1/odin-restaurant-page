a03e6e9658d3a595f3bba9fc65ce80392c3162ed
Tristan <tristanacosta03@gmail.com>
2026-06-03 20:28:40 +0800

    Refactor HTML setup to just use JavaScript DOM manipulation for home page structure

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ const image = document.createElement("img");
 image.src = odinImage;
 
 document.body.appendChild(image);
+
+const content = document.querySelector("#content");
+const headline = document.createElement("h1");
+const someText = document.createElement("p");
+
+headline.textContent = "The TOP Restaurant";
+someText.textContent = "The home to the most scrumptious meals in the town."
+
+content.appendChild(headline);
+content.appendChild(someText);
