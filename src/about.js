b306903f10c23d14a87c40ac6c62dd2a7daa06b3
Tristan <tristanacosta03@gmail.com>
2026-06-03 21:13:45 +0800

    Create module for the about page to import to index.js

---
+++ src/about.js
@@ -0,0 +1,7 @@
+const about = document.createElement("h1");
+const aboutText = document.createElement("p");
+
+about.textContent = "About Us";
+aboutText.textContent = "Serving since 1967 with our iconic full courses meal with a wide selection of appetizers, main courses, and desserts. In the Odin Restaurant, there is something for everyone that is delicious and affordable.";
+
+export { about, aboutText };
